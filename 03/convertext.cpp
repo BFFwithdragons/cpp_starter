1a6c405c9933fadcb55111aae1b186dd60a927a5
BFFwithdragons <elijahjchung@gmail.com>
2025-02-09 16:09:41 -0800

    Changed 03

---
+++ 03/convertext.cpp
@@ -0,0 +1,28 @@
+#include "../PPPheaders.h"
+// this file is just an example and is meant to be deleted
+
+int main(){
+    string input;
+    cin >> input;
+
+    if(input == "1") cout << "One";
+    else if(input == "2") cout << "Two";
+    else if(input == "3") cout << "Three";
+    else if(input == "4") cout << "Four";
+    else if(input == "5") cout << "Five";
+    else if(input == "6") cout << "Six";
+    else if(input == "7") cout << "Seven";
+    else if(input == "8") cout << "Eight";
+    else if(input == "9") cout << "Nine";
+    else if(input == "One") cout << 1;
+    else if(input == "Two") cout << 2;
+    else if(input == "Three") cout << 3;
+    else if(input == "Four") cout << 4;
+    else if(input == "Five") cout << 5;
+    else if(input == "Six") cout << 6;
+    else if(input == "Seven") cout << 7;
+    else if(input == "Eight") cout << 8;
+    else if(input == "Nine") cout << 9;
+    cout << endl;
+    return 0;
+}
